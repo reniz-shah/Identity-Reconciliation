78d710da129c6fcded5df65fd346005fe5e80bcc
reniz-shah <reniz2810@gmail.com>
2024-07-11 17:56:09 +0530

    add configurations for database

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Iroot_return } from './app.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): Iroot_return {
     return this.appService.getHello();
   }
 }
